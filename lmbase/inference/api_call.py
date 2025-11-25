bbbeabe5f84eab9ddafb539024538a9882cb93a3
YuqunZhang <zhangyuqun0909@gmail.com>
2025-11-25 21:31:41 +0800

    feat: add the function of aihubmix_api_call

--- lmbase/inference/api_call.py
+++ lmbase/inference/api_call.py
@@ -1,9 +1,39 @@
 """LLM client builder for unified provider selection."""
 
 import os
+import random
+from openai import OpenAI
+from dotenv import load_dotenv
 from typing import Optional
-
 from langchain_openai import ChatOpenAI
+
+
+def aihubmix_api_call(prompt: str, model_id: str) -> str:
+
+    load_dotenv()
+    AIHUBMIX_API_KEY = os.getenv("AIHUBMIX_API_KEY")
+    AIHUBMIX_BASE_URL = os.getenv("AIHUBMIX_BASE_URL")
+
+    client = OpenAI(
+        base_url=AIHUBMIX_BASE_URL,
+        api_key=AIHUBMIX_API_KEY,
+    )
+
+    completion = client.chat.completions.create(
+        model=model_id,  # model ID, copy from https://aihubmix.com/models
+        messages=[
+            # {"role": "developer", "content": ""},
+            {"role": "user", "content": prompt},
+        ],
+        temperature=0.1,
+        max_tokens=1024,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        seed=random.randint(1, 1000000000),
+    )
+
+    return completion.choices[0].message.content
 
 
 def build_llm(
