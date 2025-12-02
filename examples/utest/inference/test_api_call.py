48558ecf6aefcf3907163c47d6b8f99a6de93656
YuqunZhang <zhangyuqun0909@gmail.com>
2025-12-02 14:26:24 +0800

    feat: add the test of Doubao

--- examples/utest/inference/test_api_call.py
+++ examples/utest/inference/test_api_call.py
@@ -8,16 +8,35 @@ from dotenv import load_dotenv
 
 if __name__ == "__main__":
     load_dotenv()
-    api_call = LangChainAPIInference(
+
+    print("Testing LangChainAPIInference with AIHubMix...")
+    aihubmix_api_call = LangChainAPIInference(
         lm_provider="aihubmix", lm_name="DeepSeek-V3.2-Exp"
     )
-
     chatbot = InferInput(
         system_msg="you are a repeater, you should replay what the xiaoming say",
         user_msg="xiaoming said:{xiaoming_words}",
     )
+    result = aihubmix_api_call.run(chatbot, xiaoming_words="ni hao")
+    print("prompt:", result.prompt)
+    print("======")
+    print("response:", result.response)
+    print("======")
+    print("raw_response:", result.raw_response)
+    print("======")
+    print("cost:", result.cost)
 
-    result = api_call.run(chatbot, xiaoming_words="ni hao")
+    print("======")
+
+    print("Testing LangChainAPIInference with Doubao...")
+    aihubmix_api_call = LangChainAPIInference(
+        lm_provider="doubao", lm_name="doubao-seed-1-6-251015"
+    )
+    chatbot = InferInput(
+        system_msg="you are a repeater, you should replay what the xiaoming say",
+        user_msg="xiaoming said:{xiaoming_words}",
+    )
+    result = aihubmix_api_call.run(chatbot, xiaoming_words="ni hao")
     print("prompt:", result.prompt)
     print("======")
     print("response:", result.response)
