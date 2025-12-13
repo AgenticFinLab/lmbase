676796a3e58cd3d354227901d08644e068dc492f
Yuxuan Z <zyx8986@hrbeu.edu.cn>
2025-12-13 19:49:07 +0800

    fix: slightly revise the prompt

--- lmbase/identifier.py
+++ lmbase/identifier.py
@@ -6,7 +6,5 @@ MATH_SOLUTION_FLAG = "\\boxed{...}"
 OPTION_SOLUTION_FLAG = "\\boxed{...}"
 FINAL_SOLUTION_FLAG = "\\boxed{...}"
 
-MATH_SOLUTION_PROMPT = (
-    f"Please think step by step and place final solution within {MATH_SOLUTION_FLAG})."
-)
-OPTION_SOLUTION_PROMPT = f"(Please think step by step and place final selected option within {OPTION_SOLUTION_FLAG})."
+MATH_SOLUTION_PROMPT = f"\n\nPlease think step by step and place final solution within {MATH_SOLUTION_FLAG}."
+OPTION_SOLUTION_PROMPT = f"\n\nPlease think step by step and place final selected option within {OPTION_SOLUTION_FLAG}."
